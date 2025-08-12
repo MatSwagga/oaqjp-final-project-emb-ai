d51d34ee8d741023e8f102e2e6235d9497bc53c6
MatSwagga <mat-swagga@live.com>
2025-08-11 23:13:18 -0400

    update

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests #import to handle HTTP requests
 '''
 Method takes 'text_to_analyze' and runs 
@@ -19,7 +20,25 @@ def emotion_detector(text_to_analyze):
     #JSON object
     myobj = {"raw_document":{"text": text_to_analyze}}
     response = requests.post(url,json=myobj,headers=headers)
-    return response.text
+    #formatted_response is the main dictionary returned by your API call,
+    # converted from JSON format into a Python dictionary.
+    formatted_response = json.loads(response.text)
+    emotions_data = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = emotions_data['anger']
+    disgust_score = emotions_data['disgust']
+    fear_score = emotions_data['fear']
+    joy_score = emotions_data['joy']
+    sadness_score = emotions_data['sadness']    
+    dominant_emotion = max(emotions_data, key=emotions_data.get)
+    
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
 
 
     
