5cdb7475ab7a660631a41a8acc88613971bb6577
MatSwagga <mat-swagga@live.com>
2025-08-11 20:09:53 -0400

    initial commit

---
+++ emotion_detection.py
@@ -0,0 +1,25 @@
+import requests #import to handle HTTP requests
+'''
+Method takes 'text_to_analyze' and runs 
+it through an emotion dection. 
+Then the method returns the result in
+json object
+'''
+def emotion_detector(text_to_analyze):
+    #Url to access Watson NLP
+    url = (
+        'https://sn-watson-emotion.labs.skills.'
+        'network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    )
+    #Headers to access Watson NLP
+    headers = {
+        "grpc-metadata-mm-model-id":
+         "emotion_aggregated-workflow_lang_en_stock"
+    }
+    #JSON object
+    myobj = {"raw_document":{"text": text_to_analyze}}
+    response = requests.post(url,json=myobj,headers=headers)
+    return response.text
+
+
+    
